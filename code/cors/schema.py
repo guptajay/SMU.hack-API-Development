a0d80cdafde0bebf2ca12ac824ceacd2ad55d998
Jay Gupta <jay002@e.ntu.edu.sg>
2024-06-27 22:05:52 +0800

    update all requirements

--- code/cors/schema.py
+++ code/cors/schema.py
@@ -3,7 +3,7 @@ from database import Base, engine
 from sqlalchemy import Column, String, Integer
 
 """
-NTUOSS Member Schema
+SMU .hack Member Schema
 
 We represent our database members table with a Python class, which inherits from the Base class
 This allows SQLAlchemy to detect and map the class to a database table.
